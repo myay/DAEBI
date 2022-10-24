a65d38ab25ba1b8631b56d0be4a5de729847292b
Mikail <mikail.yayla@tu-dortmund.de>
2022-10-24 18:31:27 +0200

    XNOR gate implementation and test

---
+++ xnor/all_steps.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+tb_filename="xnor_gate_tb"
+
+ghdl -s $@
+if [ $? -eq 0 ]; then
+    echo Syntax check ok
+else
+    exit 1
+fi
+
+ghdl -a $@
+if [ $? -eq 0 ]; then
+    echo Analysis ok
+else
+    exit 1
+fi
+
+ghdl -e ${tb_filename}
+if [ $? -eq 0 ]; then
+    echo Build ok
+else
+    exit 1
+fi
+
+ghdl -r ${tb_filename} --vcd=testbench.vcd
+if [ $? -eq 0 ]; then
+    echo VCD dump ok
+else
+    exit 1
+fi
+
+echo "---Success---"
+
+sleep 1
+gtkwave testbench.vcd
