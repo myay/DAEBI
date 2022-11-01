98b30dea7c7ba7ea343863e5aac5947cef0e98f3
Mikail <mikail.yayla@tu-dortmund.de>
2022-11-01 12:35:57 +0100

    Clock process in popcount testbench

--- popcount/popcount_tb.vhdl
+++ popcount/popcount_tb.vhdl
@@ -19,9 +19,13 @@ architecture test of popcount_tb is
     );
   end component;
 
-signal i_val_t, clk_t, rst_t, o_val_t: std_logic;
+signal i_val_t, rst_t, o_val_t: std_logic;
+signal clk_t: std_logic := '0';
+constant clk_period : time := 2 ns;
 signal input_t: std_logic_vector(63 downto 0);
 signal output_t: std_logic_vector(13 downto 0);
+shared variable i: integer := 0;
+shared variable max_clock_cyles: integer := 20;
 
 begin
   popcount_test: popcount
@@ -35,77 +39,26 @@ begin
     );
   process begin
     i_val_t <= '0';
-    clk_t <= '0';
     rst_t <= '0';
-    wait for 10 ns;
+    wait for 1 ns;
 
     i_val_t <= '1';
     input_t <= x"ffffffffffffffff";
-    wait for 5 ns;
-
-    clk_t <= '1';
-    wait for 10 ns;
-
-    clk_t <= '0';
-    wait for 1 ns;
-
-    clk_t <= '1';
-    wait for 1 ns;
-
-    clk_t <= '0';
-    wait for 1 ns;
-
-    clk_t <= '1';
-    wait for 1 ns;
-
-    clk_t <= '0';
-    wait for 1 ns;
-
-    clk_t <= '1';
-    wait for 1 ns;
-
-    clk_t <= '0';
-    wait for 1 ns;
-
-    clk_t <= '1';
-    wait for 1 ns;
-
-    clk_t <= '0';
-    wait for 1 ns;
-
-    clk_t <= '1';
-    wait for 1 ns;
-
-    clk_t <= '0';
-    wait for 1 ns;
-
-    clk_t <= '1';
-    wait for 1 ns;
-
-    clk_t <= '0';
-    wait for 1 ns;
-
-    clk_t <= '1';
-    wait for 1 ns;
-
-    clk_t <= '0';
-    wait for 1 ns;
-
-    clk_t <= '1';
-    wait for 1 ns;
-
-    clk_t <= '0';
-    wait for 1 ns;
-
-    clk_t <= '1';
-    wait for 1 ns;
-
-    clk_t <= '0';
-    wait for 1 ns;
-
-    clk_t <= '1';
-    wait for 1 ns;
-
+    wait for 50 ns;
     wait;
   end process;
+
+  -- clock generation process
+  clk_process: process
+    begin
+      while i<max_clock_cyles loop
+        -- clk_t <= not clk_t after clk_period/2;
+        clk_t <= '0';
+        wait for clk_period/2;  -- Signal is '0'.
+        clk_t <= '1';
+        wait for clk_period/2;  -- Signal is '1'
+        i := i+1;
+      end loop;
+      wait;
+    end process;
 end test;
