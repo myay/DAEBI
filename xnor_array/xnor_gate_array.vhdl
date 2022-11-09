273ef1bd53ca04223c3f98b695638e71f8a91542
Mikail <mikail.yayla@tu-dortmund.de>
2022-11-09 14:15:38 +0100

    Generics for xnor_array

--- xnor_array/xnor_gate_array.vhdl
+++ xnor_array/xnor_gate_array.vhdl
@@ -1,22 +1,20 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-library work;
-use work.pkg.all;
-
 entity xnor_gate_array is
+  generic(nr_xnor_gates: integer);
   port(
-    xnor_inputs_1 : in std_logic_vector(xnor_gates_per_column-1 downto 0); -- First inputs
-    xnor_inputs_2 : in std_logic_vector(xnor_gates_per_column-1 downto 0); -- Second inputs
-    xnor_outputs  : out std_logic_vector(xnor_gates_per_column-1 downto 0) -- XNOR results
+    xnor_inputs_1 : in std_logic_vector(nr_xnor_gates-1 downto 0); -- First inputs
+    xnor_inputs_2 : in std_logic_vector(nr_xnor_gates-1 downto 0); -- Second inputs
+    xnor_outputs  : out std_logic_vector(nr_xnor_gates-1 downto 0) -- XNOR results
   );
 end xnor_gate_array;
 
 architecture rtl of xnor_gate_array is
-signal xnor_o : std_logic_vector(xnor_gates_per_column-1 downto 0);
+signal xnor_o : std_logic_vector(nr_xnor_gates-1 downto 0);
 begin
   -- Create a certain number of XNOR gates, number specified in xnor_gates_per_column
-  xnor_gen: for i in 0 to xnor_gates_per_column-1 generate
+  xnor_gen: for i in 0 to nr_xnor_gates-1 generate
     inst_xnor : entity work.xnor_gate(rtl)
       port map(
         xnor_in_1 => xnor_inputs_1(i),
