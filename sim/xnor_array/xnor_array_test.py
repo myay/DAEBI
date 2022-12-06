2b52a643848e12cdf221288a12c6ece94637ca8b
Mikail <mikail.yayla@tu-dortmund.de>
2022-12-06 23:01:50 +0100

    Include corner cases for testing xnor array

--- sim/xnor_array/xnor_array_test.py
+++ sim/xnor_array/xnor_array_test.py
@@ -3,14 +3,38 @@ from cocotb.triggers import Timer
 
 import random
 
+nr_xnor_gates = 4
+max_xnor_val = (2**(nr_xnor_gates))-1
+
+# corner cases test
 @cocotb.test()
-async def xnor_array_test(dut):
+async def xnor_array_test_cornercases(dut):
     """Test XNOR Array"""
 
-    nr_xnor_gates = 4
-    max_xnor_val = (2**(nr_xnor_gates))-1
+    xnor_inputs_1 = [0, max_xnor_val]
+    xnor_inputs_2 = [0, max_xnor_val]
+
+    for i in range(len(xnor_inputs_1)):
+        for j in range(len(xnor_inputs_2)):
+            # Compute with python-based xor
+            xor_outputs = (xnor_inputs_1[i] ^ xnor_inputs_2[j])
+
+            dut.xnor_inputs_1.value = xnor_inputs_1[i]
+            dut.xnor_inputs_2.value = xnor_inputs_2[j]
+
+            await Timer(1, units="ns")
+
+            # Invert output of xnor array to match xor
+            xor_dut = int(~dut.xnor_outputs.value, 2)
+            assert xor_dut == xor_outputs, "Wrong XNOR output"
+
+# random test
+@cocotb.test()
+async def xnor_array_test_random(dut):
+    """Test XNOR Array"""
+
     random.seed(1)
-    test_reps = 100
+    test_reps = 1000
 
     for i in range(0, test_reps):
         xnor_inputs_1 = random.randint(0, max_xnor_val)
