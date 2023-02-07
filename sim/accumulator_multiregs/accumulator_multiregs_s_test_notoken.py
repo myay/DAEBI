97d0c15249c321b044a818d2286bb10ec5ee6d2e
Mikail <mikail.yayla@tu-dortmund.de>
2023-02-07 18:27:27 +0100

    Add test for reset of accumulator_multiregs

--- sim/accumulator_multiregs/accumulator_multiregs_s_test_notoken.py
+++ sim/accumulator_multiregs/accumulator_multiregs_s_test_notoken.py
@@ -17,6 +17,15 @@ cycles = 1000
 
 lock = Lock()
 
+async def generate_clock_param(dut, cycles_param):
+    """Generate clock pulses."""
+
+    for cycle in range(cycles_param):
+        dut.clk.value = 0
+        await Timer(1, units="ns")
+        dut.clk.value = 1
+        await Timer(1, units="ns")
+
 async def generate_clock(dut):
     """Generate clock pulses."""
 
@@ -34,16 +43,16 @@ async def check_res(dut, regs_py, addr_to_store_value, value_to_store, p):
     #     await Timer(6, units="ns")
     await Timer(6, units="ns")
     returned_value = int(dut.o_data.value)
-    print("---")
-    print("Before: Python value", regs_py)
+    # print("---")
+    # print("Before: Python value", regs_py)
     # Update python version
     async with lock:
         regs_py[addr_to_store_value] += value_to_store
         regs_py[addr_to_store_value] = regs_py[addr_to_store_value] & and_mask
-    print("address", addr_to_store_value)
-    print("value", value_to_store)
-    print("After: Python value", regs_py)
-    print("DUT value", returned_value)
+    # print("address", addr_to_store_value)
+    # print("value", value_to_store)
+    # print("After: Python value", regs_py)
+    # print("DUT value", returned_value)
     assert returned_value == regs_py[addr_to_store_value]
 
 async def set_inputs(dut, regs_py):
@@ -75,6 +84,22 @@ async def accumulator_multiregs_s_random_test(dut):
     dut.i_val_acc.value = int(1)
 
     # Add delay of 1 to change inputs at every rising edge
+    # await Timer(1, units="ns")
+    await cocotb.start(set_inputs(dut, regs_py))
+    await Timer(1000, units="ns")
+
+    # Test reset
+    dut.reset.value = int(1)
+    await cocotb.start(generate_clock_param(dut, 100))
+    await Timer(6, units="ns")
+    dut_result = int(dut.o_data.value)
+    assert dut_result == int(0), "Reset did not work as expected"
+
+    for i in range(len(regs_py)):
+        regs_py[i] = 0
+
+    # Run tests again, with reset set to 0, but with less cycles
+    dut.reset.value = int(0)
     # await Timer(1, units="ns")
     await cocotb.start(set_inputs(dut, regs_py))
     await Timer(1000, units="ns")
